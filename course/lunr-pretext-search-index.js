37fb51becffde292b067ab1322fc671dbb99a42d
andyeisenberg <andyjeisenberg@gmail.com>
2026-01-13 22:26:28 -0500

    Latest build deployed.

--- course/lunr-pretext-search-index.js
+++ course/lunr-pretext-search-index.js
@@ -10,6 +10,195 @@ var ptx_lunr_docs = [
   "body": " Syllabus        Course Information     Day and Time  Lectures: TR 11:40 am - 1:20 pm Gladfelter L024    Recitation  Section 1: M 12:00 pm - 12:50 pm Wachman 9  Section 2: W 12:00 pm - 12:50 pm Wachman 15  Section 3: F 11:00 am - 11:50 am Wachman 408  Section 4: F 1:00 pm - 1:50 pm Wachman 408    Office Hours  tbd    Course Goals  To introduce some of the basic concepts and techniques of probability and statistics, as applied in the Life and Environmental Sciences.    Topics Covered  Probability theory and descriptive statistics, probability models, introduction to inferential statistics.    Prerequisite  MATH 1041 (C), MATH 1038 (C), MATH 1941 (C), MATH 1951 (C), MATH 2043 to 3080 (C-), MA06 Y.    Course Mode  In-person    Credits  4       Textbook and Other Required Materials     Textbook  Course notes are freely available online .    Calculator  You will need a scientific calculator (NOT a graphing calculator and NOT a calculator app on your phone\/watch). Any calculator capable of symbolic differentiation\/integration or performing statistical tests is NOT allowed. Scientific calculators (such as the TI-30XIIS ) typically cost around $10.    Phone  You will need a device (phone or tablet) to take pictures of your quizzes\/tests and upload your work.       Assessments   Homework  There will be homework assignments posted on Canvas approximately weekly. There are more practice problems available in the course notes. Doing homework problems is the primary way that you will learn and internalize the material seen in class. Plan to spend a substantial amount of time throughout the semester doing homework problems.    Quizzes  There will be in-class quizzes every other week (on Jan 20, Feb 3, Mar 10, Mar 24, and Apr 21). Quizzes give you some practice in a test-like environment without the same grade pressure as a test. The quizzes should be a signal to you about whether you're keeping up with the pace of the course.    Exams  This course will have two midterm exams and a Final Exam. Exams are the main opportunity for you to demonstrate your understanding of course material.     Test 1  Tuesday, February 17 (in class)    Test 2  Tuesday, April 7 (in class)    Final Exam  Tuesday, May 5, 10:30 am - 12:30 pm       Makeup Policy  There are no quiz or exam makeups. The lowest quiz will be dropped and replaced with the recitation score.     Grading  The grade components and their weights are:    Component  Weight    Homework  5%    Quizzes  15%    Exam 1  24%    Exam 2  24%    Final Exam  32%    The lower cutoff for each letter grade is given in the table below.     Letter Grade  Lower Cutoff    A  93%    A-  90%    B+  87%    B  83%    B-  80%    C+  77%    C  73%    C-  70%    D+  65%    D  55%    F  0%     Attendance Policy  Attendance is expected and will be recorded. If you need to miss a class for some reason, it is your responsibility to get notes from another student for any material you've missed, and to arrange to turn in\/make up any in-class or collected work.    Attendance and Your Health  To achieve course learning goals, students must attend and participate in classes, according to the course requirements. However, if you have tested positive for or are experiencing symptoms of a contagious illness, you should not come to campus or attend in-person classes or activities. It is the student’s responsibility to contact me to create a plan for participation and engagement in the course as soon as you are able to do so, and to make a plan to complete all assignments in a timely fashion.    Expectations for Class Conduct  It is important to foster a respectful and productive learning environment that includes all students in our diverse community of learners. Our differences, some of which are outlined in the University's nondiscrimination statement, will add richness to this learning experience. Therefore, all opinions and experiences, no matter how different or controversial they may be perceived, must be respected in the tolerant spirit of academic discourse.    Disability Statement  Any student who has a need for accommodations based on the impact of a documented disability or medical condition should contact Disability Resources and Services (DRS) in Howard Gittis Student Center South, Rm 420 ( drs@temple.edu ; 215-204-1280) to request accommodations and learn more about the resources available to you. If you have a DRS accommodation letter to share with me, or you would like to discuss your accommodations, please contact me as soon as practical. I will work with you and with DRS to coordinate reasonable accommodations for all students with documented disabilities. All discussions related to your accommodations will be confidential.    Academic Freedom  Freedom to teach and freedom to learn are inseparable facets of academic freedom. The University has adopted a policy on Student and Faculty Academic Rights and Responsibilities (Policy # 03.70.02) which can be accessed here .    Add\/Drop Policy  Students will be charged for a course unless dropped by the Drop\/Add deadline date. Check the University calendar for exact dates.  During the Drop\/Add period, students may drop a course with no record of the class appearing on their transcript. Students are not financially responsible for any courses dropped during this period. In the following weeks prior to or on the withdrawal date students may withdraw from a course with the grade of \"W\" appearing on their transcript. After the withdrawal date students may not withdraw from courses. Check the University calendar for exact dates. See the full policy by clicking here .    AI Policy  The use of generative AI tools (such as ChatGPT, DALL-E, etc.) is not permitted in this class unless specifically announced for a particular assignment; therefore, any use of AI tools for work in this class may be considered a violation of Temple University's Academic Honesty policy and Student Conduct Code, since the work is not your own. The use of unauthorized AI tools will result in a grade of zero on the assignment; a second offense will be reported to the Student Conduct Board.    Incomplete Grades  The grade \"I\" (an \"incomplete\") is only given if students cannot complete the course work due to circumstances beyond their control. It is necessary for the student to have completed the majority of the course work with a passing average and to sign an incomplete contract which clearly states what is left for the student to do and the deadline by which the work must be completed. The incomplete contract must also include a default grade that will be used in case the \"I\" grade is not resolved by the agreed deadline. See the full policy by clicking here .    Student Support Services  The following academic support services are available to students:      The Math Consulting Center      Student Success Center      University Libraries      Undergraduate Research Support      Career Center      Tuttleman Counseling Services      Disability Resources and Services      If you are experiencing food insecurity or financial struggles, Temple provides resources and support. Notably, the Temple University Cherry Pantry and the Temple University Emergency Student Aid Program are in operation as well as a variety of resources from the Division of Student Affairs.    "
 },
 {
+  "id": "notes-week-01",
+  "level": "1",
+  "url": "notes-week-01.html",
+  "type": "Section",
+  "number": "",
+  "title": "Week 1",
+  "body": " Week 1   This is an outline of the topics we covered in the first week of class. These notes are not a substitute for your own note-taking. I highly recommend that you take your own notes during class. If you ever miss a class for any reason, reach out to another student in class to get a copy of their notes.    Tuesday 1\/13   Sec 1.1: Sets  In prior math courses, you mostly asked deterministic questions. Now, we need new tools to model randomness .    A toxin molecule in a cell has a certain chance each minute to leave the cell.      A patient takes a diagnostic test for a disease and wants to know the chance that they have the disease based on the test result.      The sample space , often denoted , is the set of all possible results of an experiment. A single result is called an outcome , while a collection of results is called an event .      An experiment consists of rolling a standard 6-sided die (D6). The sample space is . One possible event is , i.e., the event that the result of the roll is even.    Note: we'll use notation like D6 to indicate a 6-sided die with faces 1, 2, 3, 4, 5, 6. Similarly, for example, D4 will indicate a 4-sided die with faces 1, 2, 3, 4.    The symbol means \"is an element of\", as in .  The symbol means \"is a subset of\", as in . This means that every element of the set is also an element of the set .      Consider sets and , each contained inside . We can combine sets in a variety of ways:   Union  The union of and is the set .    Intersection  The intersection of and is the set .    Difference  The set difference  is the set .    Complement  The complement of is the set .    Empty Set  The empty set , usually written or , is the set which contains no elements.       It's useful sometimes to draw pictures called Venn diagrams representing set interactions.     Union    Venn diagram showing sets with the region representing shaded.       Intersection    Venn diagram showing sets with the region representing shaded.         Difference    Venn diagram showing sets with the region representing shaded.       Complement    Venn diagram showing set with the region representing shaded.          Sec 1.2: Probability  Next, we want to start assigning probabilities to each individual outcome so we can then find the probabilities of events.    An experiment consists of rolling a D6. The sample space is . We might assign probabilities as follows:   Distribution for a fair die        1     2     3     4     5     6      Note that we don't have to assign the same probability to each outcome. If we do, we call this distribution uniform . If we have some event, such as , then we calculate the probability of the event by adding together the probabilities of the outcomes that make up the event: Note that this is the same result that we would get if we counted the total number of outcomes in and divided by the total number of outcomes in .      Given a finite set , the cardinality of , written , is the number of elements in .      If is a finite probability space with the uniform distribution and is an event, then:       Suppose we have a weighted die that's much more likely to come up 6 than any other outcome.   Distribution for a fair die        1  0.1    2  0.1    3  0.1    4  0.2    5  0.1    6  0.4     With this distribution, if , then:       Pick a number from 1 to 10. What is the probability of picking 3?  Some considerations:   Are we using the uniform distribution?    What even is the sample space here? Do we need to pick only integers, or is an outcome here?    What would \"uniform\" mean if the sample space was infinite?         A probability distribution on a sample space assigns probabilities to every event, satisfying the following conditions:    .     for any event .    If , then .         Suppose we flip a coin until we see heads. The sample space is . What would \"uniform\" mean here? Is there any way we could assign the same probability to each individual outcome here?  Instead of \"uniform\", what if we want to treat the coin as a fair coin? Then what would the distribution be?   Distribution assuming a fair coin     # flips      1      2      3      4                         One last (very useful!) observation. Recall: If , then .  For any event , and . So: We can rewrite this in two useful ways:        "
+},
+{
+  "id": "subsubsec-Sets-2",
+  "level": "2",
+  "url": "notes-week-01.html#subsubsec-Sets-2",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "deterministic randomness "
+},
+{
+  "id": "subsubsec-Sets-3",
+  "level": "2",
+  "url": "notes-week-01.html#subsubsec-Sets-3",
+  "type": "Example",
+  "number": "1",
+  "title": "",
+  "body": "  A toxin molecule in a cell has a certain chance each minute to leave the cell.   "
+},
+{
+  "id": "subsubsec-Sets-4",
+  "level": "2",
+  "url": "notes-week-01.html#subsubsec-Sets-4",
+  "type": "Example",
+  "number": "2",
+  "title": "",
+  "body": "  A patient takes a diagnostic test for a disease and wants to know the chance that they have the disease based on the test result.   "
+},
+{
+  "id": "def-sample-space",
+  "level": "2",
+  "url": "notes-week-01.html#def-sample-space",
+  "type": "Definition",
+  "number": "3",
+  "title": "",
+  "body": "  The sample space , often denoted , is the set of all possible results of an experiment. A single result is called an outcome , while a collection of results is called an event .   "
+},
+{
+  "id": "example-sample-space",
+  "level": "2",
+  "url": "notes-week-01.html#example-sample-space",
+  "type": "Example",
+  "number": "4",
+  "title": "",
+  "body": "  An experiment consists of rolling a standard 6-sided die (D6). The sample space is . One possible event is , i.e., the event that the result of the roll is even.   "
+},
+{
+  "id": "def-subset",
+  "level": "2",
+  "url": "notes-week-01.html#def-subset",
+  "type": "Definition",
+  "number": "5",
+  "title": "",
+  "body": "  The symbol means \"is an element of\", as in .  The symbol means \"is a subset of\", as in . This means that every element of the set is also an element of the set .   "
+},
+{
+  "id": "def-set-operations",
+  "level": "2",
+  "url": "notes-week-01.html#def-set-operations",
+  "type": "Definition",
+  "number": "6",
+  "title": "",
+  "body": "  Consider sets and , each contained inside . We can combine sets in a variety of ways:   Union  The union of and is the set .    Intersection  The intersection of and is the set .    Difference  The set difference  is the set .    Complement  The complement of is the set .    Empty Set  The empty set , usually written or , is the set which contains no elements.      "
+},
+{
+  "id": "subsubsec-Sets-10",
+  "level": "2",
+  "url": "notes-week-01.html#subsubsec-Sets-10",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Venn diagrams "
+},
+{
+  "id": "fig-Venn-diagram-union",
+  "level": "2",
+  "url": "notes-week-01.html#fig-Venn-diagram-union",
+  "type": "Figure",
+  "number": "7",
+  "title": "",
+  "body": " Union    Venn diagram showing sets with the region representing shaded.     "
+},
+{
+  "id": "fig-Venn-diagram-intersection",
+  "level": "2",
+  "url": "notes-week-01.html#fig-Venn-diagram-intersection",
+  "type": "Figure",
+  "number": "8",
+  "title": "",
+  "body": " Intersection    Venn diagram showing sets with the region representing shaded.     "
+},
+{
+  "id": "fig-Venn-diagram-difference",
+  "level": "2",
+  "url": "notes-week-01.html#fig-Venn-diagram-difference",
+  "type": "Figure",
+  "number": "9",
+  "title": "",
+  "body": " Difference    Venn diagram showing sets with the region representing shaded.     "
+},
+{
+  "id": "fig-Venn-diagram-complement",
+  "level": "2",
+  "url": "notes-week-01.html#fig-Venn-diagram-complement",
+  "type": "Figure",
+  "number": "10",
+  "title": "",
+  "body": " Complement    Venn diagram showing set with the region representing shaded.     "
+},
+{
+  "id": "subsubsec-Probability-3",
+  "level": "2",
+  "url": "notes-week-01.html#subsubsec-Probability-3",
+  "type": "Example",
+  "number": "11",
+  "title": "",
+  "body": "  An experiment consists of rolling a D6. The sample space is . We might assign probabilities as follows:   Distribution for a fair die        1     2     3     4     5     6      Note that we don't have to assign the same probability to each outcome. If we do, we call this distribution uniform . If we have some event, such as , then we calculate the probability of the event by adding together the probabilities of the outcomes that make up the event: Note that this is the same result that we would get if we counted the total number of outcomes in and divided by the total number of outcomes in .   "
+},
+{
+  "id": "def-cardinality",
+  "level": "2",
+  "url": "notes-week-01.html#def-cardinality",
+  "type": "Definition",
+  "number": "13",
+  "title": "",
+  "body": "  Given a finite set , the cardinality of , written , is the number of elements in .   "
+},
+{
+  "id": "subsubsec-Probability-5",
+  "level": "2",
+  "url": "notes-week-01.html#subsubsec-Probability-5",
+  "type": "Fact",
+  "number": "14",
+  "title": "",
+  "body": "  If is a finite probability space with the uniform distribution and is an event, then:    "
+},
+{
+  "id": "subsubsec-Probability-6",
+  "level": "2",
+  "url": "notes-week-01.html#subsubsec-Probability-6",
+  "type": "Example",
+  "number": "15",
+  "title": "",
+  "body": "  Suppose we have a weighted die that's much more likely to come up 6 than any other outcome.   Distribution for a fair die        1  0.1    2  0.1    3  0.1    4  0.2    5  0.1    6  0.4     With this distribution, if , then:    "
+},
+{
+  "id": "subsubsec-Probability-7",
+  "level": "2",
+  "url": "notes-week-01.html#subsubsec-Probability-7",
+  "type": "Example",
+  "number": "17",
+  "title": "",
+  "body": "  Pick a number from 1 to 10. What is the probability of picking 3?  Some considerations:   Are we using the uniform distribution?    What even is the sample space here? Do we need to pick only integers, or is an outcome here?    What would \"uniform\" mean if the sample space was infinite?      "
+},
+{
+  "id": "def-probability-distribution",
+  "level": "2",
+  "url": "notes-week-01.html#def-probability-distribution",
+  "type": "Definition",
+  "number": "18",
+  "title": "",
+  "body": "  A probability distribution on a sample space assigns probabilities to every event, satisfying the following conditions:    .     for any event .    If , then .      "
+},
+{
+  "id": "subsubsec-Probability-9",
+  "level": "2",
+  "url": "notes-week-01.html#subsubsec-Probability-9",
+  "type": "Example",
+  "number": "19",
+  "title": "",
+  "body": "  Suppose we flip a coin until we see heads. The sample space is . What would \"uniform\" mean here? Is there any way we could assign the same probability to each individual outcome here?  Instead of \"uniform\", what if we want to treat the coin as a fair coin? Then what would the distribution be?   Distribution assuming a fair coin     # flips      1      2      3      4                      "
+},
+{
+  "id": "subsubsec-Probability-10",
+  "level": "2",
+  "url": "notes-week-01.html#subsubsec-Probability-10",
+  "type": "Example",
+  "number": "21",
+  "title": "",
+  "body": "  One last (very useful!) observation. Recall: If , then .  For any event , and . So: We can rewrite this in two useful ways:    "
+},
+{
   "id": "recitation-calculus-review",
   "level": "1",
   "url": "recitation-calculus-review.html",
@@ -59,7 +248,7 @@ var ptx_lunr_docs = [
   "level": "2",
   "url": "recitation-calculus-review.html#thm-FTC",
   "type": "Theorem",
-  "number": "1",
+  "number": "22",
   "title": "Fundamental Theorem of Calculus.",
   "body": " Fundamental Theorem of Calculus   If is an antiderivative of i.e., if then:    "
 },
